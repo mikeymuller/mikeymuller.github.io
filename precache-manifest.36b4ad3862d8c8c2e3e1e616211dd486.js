c98ee03fbcb1cc440ba2aa5d52cc47d6a04486c4
Mikey Muller <michaelmuller799@gmail.com>
2019-12-17 22:36:40 -0800

    Updates

---
+++ precache-manifest.36b4ad3862d8c8c2e3e1e616211dd486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1563f056a8e2ce901951e139758ca6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52c9dfaf7aa0d4f9427d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "584ca739afbbb8973fb9",
+    "url": "/static/js/2.9e5e6cfd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9e5e6cfd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52c9dfaf7aa0d4f9427d",
+    "url": "/static/js/main.9ad8384b.chunk.js"
+  },
+  {
+    "revision": "f5872a621ceddbb4a534",
+    "url": "/static/js/runtime-main.cab9f41d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
